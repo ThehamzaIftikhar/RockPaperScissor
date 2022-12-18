55dd136390dd0a18f726a3a5aaa9d79150df8976
Hamza Iftikhar <thehamzaiftikhar@gmail.com>
2022-12-18 10:44:17 -0500

    Resolve all errors

--- javascript.js
+++ javascript.js
@@ -2,29 +2,29 @@ game();
 
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3)) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissor";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissor";
     }
 }
 
-function playRound(playerSelection, ComputerSelection) {
-    playerSelection = playerSelection.toLowercase();
-    if (ComputerSelection === "rock") {
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection === "rock") {
         switch(playerSelection) {
             case "rock": return "It's a tie";
             case "paper": return "Player wins";
             case "scissor": return "Computer wins";
         }
     }
-    if (ComputerSelection === "paper") {
+    if (computerSelection === "paper") {
         switch(playerSelection) {
             case "paper": return "It's a tie";
             case "scissor": return "Player wins";
             case "rock": return "Computer wins";
         }
     }
-    if (ComputerSelection === "scissor") {
+    if (computerSelection === "scissor") {
         switch(playerSelection) {
             case "scissor": return "It's a tie";
             case "rock": return "Player wins";
@@ -34,18 +34,14 @@ function playRound(playerSelection, ComputerSelection) {
 }
 
 function getPlayerChoice() {
-    let ans;
-    while (!(ans === "rock" || ans === "paper" || ans === "scissor" )) {
-        ans = prompt("Enter rock, paper or scissor");
-    }
-    return ans;
+    return prompt("Enter Rock, Paper or Scissor");
 }
 
 function game() {
     let playerPoints = 0;
     let ComputerPoints = 0;
     for (let i = 0; i < 5; i++) {
-        let winner = playRound(getPlayerChoice, getComputerChoice)
+        let winner = playRound(getPlayerChoice(), getComputerChoice())
         console.log(winner);
         switch(winner) {
             case "Player wins": playerPoints++;
@@ -55,7 +51,7 @@ function game() {
     if (playerPoints > ComputerPoints) {
         console.log("Player won the game");
     } else if (ComputerPoints > playerPoints) {
-        console.log("Compute wins the game");
+        console.log("Computer won the game");
     } else {
         console.log("The game was a tie");
     }
